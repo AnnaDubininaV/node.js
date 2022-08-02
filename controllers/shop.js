f21af07ab77f7f17df732ab479fe5cce533d8e7d
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-02 21:19:29 +0700

    Added product detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,8 +13,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => console.log(product));
-  res.redirect('/');
+  Product.findById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/products',
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
