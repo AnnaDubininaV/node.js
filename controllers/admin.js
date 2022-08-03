a4b0554e06ee6f9c0b4599f97e167d9c6c5d190d
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-03 18:20:55 +0700

    Added edit product functionality (comtroller, model, view)

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,21 +2,60 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   // relative path to /views fofled
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Products',
     path: '/admin/add-product',
+    editing: false,
     // use this options only in Hbs engine
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true,
   });
 };
 
 exports.postAddProduct = (req, res) => {
   const { title, imageUrl, price, description } = req.body;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    // TODO: show an error instead
+    if (!product) {
+      return res.redirect('/');
+    }
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Products',
+      path: '/admin/add-product',
+      editing: true,
+      product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const {
+    productId,
+    title: updatedTitle,
+    price: updatedPrice,
+    imageUrl: updatedImageUrl,
+    description: updatedDescription,
+  } = req.body;
+
+  const updatedProduct = new Product(
+    productId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -15,7 +15,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -23,12 +24,26 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(productsPath, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      let updatedProducts;
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+      }
+
+      fs.writeFile(
+        productsPath,
+        JSON.stringify(updatedProducts || products),
+        (err) => {
+          console.log(err);
+        }
+      );
     });
   }
 
