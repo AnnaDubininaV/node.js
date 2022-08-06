fb78d1a2f4f6416a39556322e5cd2eb63894e41a
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-06 16:28:22 +0700

    Defined a product Model

--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ const dbConfig = require('./databaseConfig');
 
 const { HOST, USER, DB_NAME, PASSWORD } = dbConfig;
 
-// this will set up a connection pool under the hood
+// this will set up a connection pool under the hood with a fully configured sequelize environment
 const sequelize = new Sequelize(DB_NAME, USER, PASSWORD, {
   dialect: mysql,
   host: HOST,
