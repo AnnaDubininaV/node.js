080a69d23b703a73648b6bc3d6345ea4cc4c03aa
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-14 01:27:47 +0700

    added get all products functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
   // TODO refactore this method along with getIndex
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       console.log(products);
       res.render('shop/product-list', {
@@ -31,7 +31,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render('shop/index', {
         prods: products,
