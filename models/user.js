7e2562c043d4cc90b5fa554cdf1b0da07890171e
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-13 12:41:46 +0700

    Added addToCart method to the User model

--- models/user.js
+++ models/user.js
@@ -1,14 +1,31 @@
 const { getDB } = require('../utils/database');
 const mongoDB = require('mongodb');
 class User {
-  constructor(userName, email) {
+  constructor(userName, email, cart, id) {
     this.name = userName;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDB();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(productId) {
+    // const cartProduct = this.cart.items.findIndex(
+    //   (item) => item._id === productId._id
+    // );
+
+    const updatedCart = { items: [{ ...productId, quantity: 1 }] };
+    const db = getDB();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new mongoDB.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
