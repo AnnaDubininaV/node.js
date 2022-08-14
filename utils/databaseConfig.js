96399ffe47960a3092465c5008a8efddfca0f8f6
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-14 21:10:01 +0700

    Section setup

---
+++ utils/databaseConfig.js
@@ -0,0 +1,8 @@
+const CLUSTER_NAME = 'cluster0';
+const USER_NAME = 'Ana';
+const PASSWORD = 'fvLlvcTwihRpELy9';
+
+const MONGO_DB_PASSWORD = 'wf7mxVp.975CKVQ';
+const PROJECT_MONGO_DB_PASSWORD = 'fvLlvcTwihRpELy9';
+
+module.exports = { USER_NAME, CLUSTER_NAME, PASSWORD };
