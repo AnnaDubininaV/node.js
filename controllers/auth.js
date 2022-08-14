a0b8685c20bfd82de75c85396fb6a8e1824d8fb8
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-15 05:17:30 +0700

    Added Encrypting passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -41,12 +42,22 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
-      const user = new User({ email, password, cart: { items: [] } });
-      return user.save();
+
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then(() => {
+          res.redirect('/login');
+        });
     })
-    .then(() => {
-      res.redirect('/login');
-    })
+
     .catch((err) => console.log(err));
 };
 
